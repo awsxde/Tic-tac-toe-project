31d2a7595bcd836abccf9ec8af60326d9fd2fc80
awsxde <ali23khour@gmail.com>
2022-12-22 15:16:05 +0330

    cleared

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-let count = 0;
-let turn = true;
-let btn = document.querySelectorAll(".btn");
-let arr = Array.from({ length: btn.length }, (_, i) => (i = "empty"));
-let clicked = Array.from({ length: btn.length }, (_, i) => (i = false));
+//elements
+let message = document.querySelector("p");
+let body = document.querySelector("body");
+let btn = document.querySelectorAll("button");
 
-function assign(j, z) {
-  btn[j].innerHTML = `${z}`;
-  turn = !turn;
-  clicked[j] = true;
-  arr[j] = btn[j].innerHTML;
+//variables
+let side;
+let n = 0;
+let num = Array.from({ length: 9 }, (_, i) => i);
+
+//functions
+// //two players
+btn.forEach((i) =>
+  i.addEventListener("click", () => {
+    if (i.classList.contains("empty") && !isOver()) {
+      side = ["x", "o"][num[n++] % 2];
+      i.textContent = side;
+      i.classList.remove("empty");
+      check();
+    }
+  })
+);
+
+function check() {
+  isOver()
+    ? end("tie")
+    : [
+        winCheck(0, 1, 2),
+        winCheck(3, 4, 5),
+        winCheck(6, 7, 8),
+        winCheck(0, 3, 6),
+        winCheck(1, 4, 7),
+        winCheck(2, 5, 8),
+        winCheck(0, 4, 8),
+        winCheck(2, 4, 6),
+      ].some((i) => i === true) && end("win");
 }
 
-for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", function () {
-    clicked[i] && count--;
-    !clicked[i] && assign(i, turn ? "X" : "O");
-    check();
-  });
+function isOver() {
+  return document.querySelectorAll(".empty").length === 0;
 }
 
-function win(name) {
-  document.querySelector("p").textContent = `${name} wins!`;
-  document.querySelector("body").style.background = "lightgreen";
-  window.scrollTo(0, 0);
-}
-
-function tie() {
-  document.querySelector("p").textContent = `tied!`;
-  document.querySelector("body").style.background = "grey";
-  window.scrollTo(0, 0);
-}
-
-function logic(a, b, c, array) {
-  return (
-    array[a] === array[b] &&
-    array[b] === array[c] &&
-    ["X", "O"].includes(array[a])
+function winCheck(...array) {
+  return ["xxx", "ooo"].includes(
+    array.reduce((a, b) => a + btn[b].textContent, "")
   );
 }
 
-function check() {
-  logic(0, 1, 2, arr)
-    ? win(`${arr[0]}`)
-    : logic(3, 4, 5, arr)
-    ? win(`${arr[3]}`)
-    : logic(6, 7, 8, arr)
-    ? win(`${arr[6]}`)
-    : logic(0, 3, 6, arr)
-    ? win(`${arr[0]}`)
-    : logic(1, 4, 7, arr)
-    ? win(`${arr[1]}`)
-    : logic(2, 5, 8, arr)
-    ? win(`${arr[2]}`)
-    : logic(0, 4, 8, arr)
-    ? win(`${arr[0]}`)
-    : logic(2, 4, 6, arr)
-    ? win(`${arr[2]}`)
-    : count === 8
-    ? tie()
-    : count++;
+function end(x) {
+  message.textContent = x === "tie" ? "tied" : `${side} won`;
+  body.style.background = x === "tie" ? "white" : "lightgreen";
+  btn.forEach((i) => i.classList.remove("empty"));
+  // window.scrollTo(0, 0);
 }
+
+// //one player
+// let btn = document.querySelectorAll("button");
+
+// //assume player is x
+// btn.forEach((i) =>
+//   i.addEventListener("click", () => {
+//     i.textContent = "x";
+//   })
+// );
